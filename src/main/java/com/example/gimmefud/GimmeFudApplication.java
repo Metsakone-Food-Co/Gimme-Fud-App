a40f7ed6a66ce068fa7b683ee36835e0b7e7d65c
Niko Naumanen <t0nani00@students.oamk.fi>
2022-03-20 12:22:08 +0200

    Tehty uusiksi
    Herokun kiukuttelusta johtuen kaikki tehty uusiksi

---
+++ src/main/java/com/example/gimmefud/GimmeFudApplication.java
@@ -0,0 +1,13 @@
+package com.example.gimmefud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmeFudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimmeFudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gimmefud/GimmeFudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gimmefud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeFudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
