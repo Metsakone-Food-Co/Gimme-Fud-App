466a0beeaec98a100bf5025e4b97682b40798496
Niko Naumanen <t0nani00@students.oamk.fi>
2022-03-22 11:19:23 +0200

    Update MainController.java

--- src/main/java/com/example/gimmefud/MainController.java
+++ src/main/java/com/example/gimmefud/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class welcomeController  {
+public class MainController {
 
     @Autowired
     CustomerService customerService;
