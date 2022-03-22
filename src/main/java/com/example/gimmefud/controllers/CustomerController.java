20d8fd3c0124ef880b4d5077d90d2fc85ff5a528
Niko Naumanen <t0nani00@students.oamk.fi>
2022-03-22 16:20:59 +0200

    asiakkaan tiedon haku toimii

--- src/main/java/com/example/gimmefud/controllers/CustomerController.java
+++ src/main/java/com/example/gimmefud/controllers/CustomerController.java
@@ -3,12 +3,14 @@ package com.example.gimmefud.controllers;
 import com.example.gimmefud.CustomerService;
 import com.example.gimmefud.data.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
@@ -17,7 +19,7 @@ public class CustomerController {
     CustomerService customerService;
 
     @GetMapping
-    public List<Customer> getALlEmployees(){
+    public List<Customer> getAllCustomers(){
         return customerService.getcustomer() ;
     }
 
