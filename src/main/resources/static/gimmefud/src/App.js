9b9202201cd49494903e739dbe68f2f26ecedb58
JaakkoMal <j.j.malmi@gmail.com>
2022-03-24 14:52:35 +0200

    create-owner-and-owner-list-done
    Ravintoloitsija listaus ja ravintoloitsijan luominen tehty.

--- src/main/resources/static/gimmefud/src/App.js
+++ src/main/resources/static/gimmefud/src/App.js
@@ -2,8 +2,10 @@
 import './App.css';
 import HomepageComponent from './components/HomepageComponent'
 import ListCustomerComponent from './components/ListCustomerComponent'
+import ListOwnerComponent from './components/ListOwnerComponent'
 import ListRestaurantComponent from './components/ListRestaurantComponent'
 import CreateCustomerComponent from './components/CreateCustomerComponent'
+import CreateOwnerComponent from './components/CreateOwnerComponent'
 import CreateRestaurantComponent from './components/CreateRestaurantComponent'
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
@@ -21,14 +23,18 @@ function App() {
         <Link to="/"><div>Home</div></Link>
         <Link to="/ListRestaurantComponent"><div>Restaurant list</div></Link>
         <Link to="/CreateCustomerComponent"><div>Create customer</div></Link>
+        <Link to="/CreateOwnerComponent"><div>Create owner</div></Link>
         <Link to="/CreateRestaurantComponent"><div>Create restaurant</div></Link>
         <Link to="/ListCustomerComponent"><div>Customer list</div></Link>
+        <Link to="/ListOwnerComponent"><div>Restaurant owners</div></Link>
       </div>
       <Routes>
         <Route path="/" element={ <HomepageComponent/>}/>
         <Route path="ListCustomerComponent" element={<ListCustomerComponent/>}/>
         <Route path="ListRestaurantComponent" element={<ListRestaurantComponent/>}/>
+        <Route path="ListOwnerComponent" element={<ListOwnerComponent/>}/>
         <Route path="CreateCustomerComponent" element={<CreateCustomerComponent/>}/>
+        <Route path="CreateOwnerComponent" element={<CreateOwnerComponent/>}/>
         <Route path="CreateRestaurantComponent" element={<CreateRestaurantComponent />}/>
 
       </Routes>
