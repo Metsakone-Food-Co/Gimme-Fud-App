a8b4ed940b85d2e05499aa21751aa60b51c333dc
Trinkku <trinh.leloan@hotmail.com>
2022-03-28 04:48:41 +0300

    Uusia toimintoja
    ListofRestauranttiin kokeiltu pääkuva, ja ikonit/logot

--- src/main/resources/static/gimmefud/src/components/ListRestaurantComponent.js
+++ src/main/resources/static/gimmefud/src/components/ListRestaurantComponent.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 import RestaurantService from '../services/RestaurantService';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Button} from "react-bootstrap";
+import {Card, Button, Badge} from "react-bootstrap";
 import Row from 'react-bootstrap/Row'
-import { BsCartPlus } from "react-icons/bs";
+import { BsCartPlus} from "react-icons/bs";
+import { MdFastfood } from "react-icons/md";
+
+
 
 
 
@@ -32,35 +35,42 @@ const ListRestaurantComponent = () => {
   return (
     <div className="container">
       
-
-        <h3>List of Restaurants</h3>
+       <div class="card text-white">
+       <img src="ruokakuva.png" class="card-img" alt= "background" width="100%" height="400px"/>
+       <div class="card-img-overlay">
+       <h3>List of Restaurants</h3>
+   
+       </div>
+       </div>
        
-      
+
         <Row xs={1} md={3} className="g-4">
           {restaurants.map(restaurant =>(
             <tr key={restaurant.rname}>
                 
                 <Card style={{ width: '18rem'  }} >
                 <Card.Body style={{border: '50px'}}>
-                <Card.Header>{restaurant.rname}</Card.Header>
               <Card.Img variant="top" src="https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/w_1280,c_limit/the-ultimate-hamburger.jpg"/>
-              <Card.Title>{restaurant.rype}</Card.Title>
-              <Card.Text>
+            
+             <Card.Title  >{restaurant.rname} <BsCartPlus/></Card.Title>
 
-              <p>Opening hours: {restaurant.service_hours} <BsCartPlus/> </p>
-              
-                    
-              
-              </Card.Text>
-              <Card.Text></Card.Text>
               <Card.Text>
-                
-              <p>Price Range: {restaurant.price_range}  </p>   
-              
+              <p>Opening hours: {restaurant.service_hours}</p> </Card.Text>
+              <Card.Text>
+              <p>Price Range: {restaurant.price_range} </p> 
               </Card.Text>
-  
-              <button  type="button" class="btn btn-outline-secondary"> Tilaa <BsCartPlus/></button>
-              <Card.Link href="#">Give Feedback</Card.Link>
+              
+      
+                <button type="button" class="btn btn-outline-secondary"> Tilaa <BsCartPlus/></button>
+                <Card.Link href="#">Give Feedback</Card.Link>
+                <Card.Text>
+              
+                <Badge bg="light" text="info">{restaurant.rtype}<MdFastfood/></Badge>
+                  
+                </Card.Text>
+           
+                
+             
      
               </Card.Body>
               </Card>
@@ -69,8 +79,10 @@ const ListRestaurantComponent = () => {
           ))
           }
           </Row>
+       <div class="downcont">
+      
+       </div>
        
-    
         
       </div>
  
