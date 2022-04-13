d68e09026856e93c30668748225b566718805fe7
Trinkku <trinh.leloan@hotmail.com>
2022-04-13 22:04:09 +0300

    Update OrderSuccesfull.js

--- src/main/resources/gimmefud/src/Messages/OrderSuccesfull.js
+++ src/main/resources/gimmefud/src/Messages/OrderSuccesfull.js
@@ -4,7 +4,7 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 export default function OrderSuccesfull() {
   return (
       <div className="container">
-    <h1>Thank you, your order is made succesfull !</h1>
+    <h1>Thank you, your order is made succesfully !</h1>
     <Link to="/"><button type="button" class="btn btn-primary btn-lg ">Back to homepage</button></Link>
     </div>
   )
