d46c1298c23c1389a63af9f4d28714e8e092c91f
Niko Naumanen <t0nani00@students.oamk.fi>
2022-04-18 09:08:59 +0300

    viimeinen build

--- src/main/resources/gimmefud/src/http-common.js
+++ src/main/resources/gimmefud/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL: "https://gimme-fud-app.herokuapp.com/api/v1",
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: "https://gimme-fud-app.herokuapp.com/api/v1",
+    //baseURL: 'http://localhost:8080/api/v1',
     headers: {
         'Content-Type': 'application/json'
     }
